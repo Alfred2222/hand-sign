bd7c78712bc1726fb6d3eb4bbe5b0d995bf98b43
Alfred2222 <84137430+Alfred2222@users.noreply.github.com>
2022-09-14 15:59:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+})
+
+camera=document.getElementById("camera")
+Webcam.attach(camera);
+
+function capture_img(){
+Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML="<img id='captured_img' src='"+data_uri+"'>"
+})
+}
+
+console.log('ml5 version',ml5.version)
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/UwSTlzY65/model.json',modelLoaded)
+
+function modelLoaded(){
+    console.log("model loaded")
+}
+
+function speak(){
+synth=window.speechSynthesis;
+speak_data1="The first Prediction is"+prediction1
+speak_data2="the second prediction is"+prediction2
+utterthis=new SpeechSynthesisUtterance(speak_data1+speak_data2);
+synth.speak(utterthis)
+}
